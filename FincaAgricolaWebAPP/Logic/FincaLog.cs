177639fd8634cad62be674d6eb002d5bbc3a5b12
Andresgonzalez06 <andresfeliperg049@gmail.com>
2024-10-28 12:10:34 -0500

    Se crearon las funcionalidades para el (CRUD) de FincaLog.cs en la capa logica

---
+++ FincaAgricolaWebAPP/Logic/FincaLog.cs
@@ -0,0 +1,33 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Web;
+
+
+namespace Logic
+{
+    public class FincaLog
+    {
+        FarmDat objFin = new FarmDat();
+        public DataSet showFarm()
+        {
+            return objFin.showFarm();
+        }
+        public DataSet showFarmDDL()
+        {
+            return objFin.showFarmDDL();
+        }
+        public bool saveFarm(string _nombre, string _ubicacion)
+        {
+            return objFin.saveFarm(_nombre, _ubicacion);
+        }
+        public bool deleteFarm(int _idFarm)
+        {
+            return objFin.deleteFarm(_idFarm);
+        }
+    }  
+}
