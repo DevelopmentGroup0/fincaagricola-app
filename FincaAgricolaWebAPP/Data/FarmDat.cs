06930ad646193d9826e892c1c70515244f0a4416
Jabuitron <Jandresbuiitron@gmail.com>
2024-11-10 01:45:56 -0500

    FA-57 Se creó WF con las funcionalidades CRUD para tbl_proveedor

--- FincaAgricolaWebAPP/Data/FarmDat.cs
+++ FincaAgricolaWebAPP/Data/FarmDat.cs
@@ -34,7 +34,7 @@ namespace Data
 
             MySqlCommand objSelectCmd = new MySqlCommand();
             objSelectCmd.Connection = objPer.openConnection();
-            objSelectCmd.CommandText = "procSelectFinca";
+            objSelectCmd.CommandText = "procSelectFincaDDL";
             objSelectCmd.CommandType = CommandType.StoredProcedure;
             objAdapter.SelectCommand = objSelectCmd;
             objAdapter.Fill(objData);
