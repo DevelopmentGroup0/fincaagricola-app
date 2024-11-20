6e456b767f37994bb3c4eedcfb8c560571884ffa
Alejandra Gaviria <alejandragaviria.190@gmail.com>
2024-11-20 13:03:09 -0500

    se agregaron estilos a los label y TB

--- FincaAgricolaWebAPP/Presentation/WFCrop.aspx.cs
+++ FincaAgricolaWebAPP/Presentation/WFCrop.aspx.cs
@@ -40,7 +40,7 @@ namespace Presentation
         {
             DDLParcel.DataSource = objPar.showParcelDDL();
             DDLParcel.DataValueField = "par_id";
-            DDLParcel.DataTextField = "nombre";
+            DDLParcel.DataTextField = "ubicacionParcela";
             DDLParcel.DataBind();
             DDLParcel.Items.Insert(0, "Seleccione");
         }
--- FincaAgricolaWebAPP/Presentation/WFCrop.aspx.designer.cs
+++ FincaAgricolaWebAPP/Presentation/WFCrop.aspx.designer.cs
@@ -24,15 +24,6 @@ namespace Presentation
         protected global::System.Web.UI.WebControls.HiddenField HFCultivolId;
 
         /// <summary>
-        /// Control Label1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
-
-        /// <summary>
         /// Control TBNombre.
         /// </summary>
         /// <remarks>
@@ -40,15 +31,6 @@ namespace Presentation
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TBNombre;
-
-        /// <summary>
-        /// Control Label2.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label2;
 
         /// <summary>
         /// Control TBDescripcion.
