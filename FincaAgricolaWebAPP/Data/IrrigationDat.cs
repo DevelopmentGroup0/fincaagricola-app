e5835bff5d73e6cfa7b748208bbe57a04e85a514
Jabuitron <Jandresbuiitron@gmail.com>
2024-11-19 20:43:01 -0500

    Implementación de BT + FW con funcionalidades CRUD para tbl_riego

--- FincaAgricolaWebAPP/Data/IrrigationDat.cs
+++ FincaAgricolaWebAPP/Data/IrrigationDat.cs
@@ -17,26 +17,41 @@ namespace Data
             DataSet objData = new DataSet();
             MySqlCommand objSelectCmd = new MySqlCommand();
             objSelectCmd.Connection = objPer.openConnection();
-            objSelectCmd.CommandText = "procSelectWeather";
+            objSelectCmd.CommandText = "procSelecIrrigation";
             objSelectCmd.CommandType = CommandType.StoredProcedure;
             objAdapter.SelectCommand = objSelectCmd;
             objAdapter.Fill(objData);  
             objPer.closeConnection();
             return objData;
         }
+
+        public DataSet showIrrigationDDL()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "procSelectWeatherDLL";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+            objPer.closeConnection();
+            return objData;
+        }
+
         public bool saveIrrigation(string _tipo, string _cantidad, string _frecuencia, int _fkCultivoId, int _fkParId)
         {            
             bool executed = false;
             int row;
             MySqlCommand objSelectCmd = new MySqlCommand();
             objSelectCmd.Connection = objPer.openConnection();
-            objSelectCmd.CommandText = "spInsertIrrigation"; 
+            objSelectCmd.CommandText = "procInsertIrrigation"; 
             objSelectCmd.CommandType = CommandType.StoredProcedure;
-            objSelectCmd.Parameters.Add("rie_tipo", MySqlDbType.VarString).Value = _tipo;
-            objSelectCmd.Parameters.Add("rie_cantidad", MySqlDbType.VarString).Value = _cantidad;
-            objSelectCmd.Parameters.Add("rie_frecuencia", MySqlDbType.VarString).Value = _frecuencia;
-            objSelectCmd.Parameters.Add("tbl_cultivo_cul_id", MySqlDbType.Int32).Value = _fkCultivoId;
-            objSelectCmd.Parameters.Add("tbl_cultivo_tbl_parcela_par_id", MySqlDbType.Int32).Value = _fkParId;
+            objSelectCmd.Parameters.Add("v_tipo", MySqlDbType.VarString).Value = _tipo;
+            objSelectCmd.Parameters.Add("v_cantidad_agua", MySqlDbType.VarString).Value = _cantidad;
+            objSelectCmd.Parameters.Add("v_frecuencia", MySqlDbType.VarString).Value = _frecuencia;
+            objSelectCmd.Parameters.Add("vfk_cultivo_id", MySqlDbType.Int32).Value = _fkCultivoId;
+            objSelectCmd.Parameters.Add("vfk_tbl_par_id", MySqlDbType.Int32).Value = _fkParId;
             try
             {
                 row = objSelectCmd.ExecuteNonQuery();
@@ -58,14 +73,14 @@ namespace Data
             int row;
             MySqlCommand objSelectCmd = new MySqlCommand();
             objSelectCmd.Connection = objPer.openConnection();
-            objSelectCmd.CommandText = "spUpdateIrrigation"; 
+            objSelectCmd.CommandText = "procUpdateIrrigation"; 
             objSelectCmd.CommandType = CommandType.StoredProcedure;
-            objSelectCmd.Parameters.Add("rie_id", MySqlDbType.Int32).Value = _idRiego;
-            objSelectCmd.Parameters.Add("rie_tipo", MySqlDbType.VarString).Value = _tipo;
-            objSelectCmd.Parameters.Add("rie_cantidad", MySqlDbType.VarString).Value = _cantidad;
-            objSelectCmd.Parameters.Add("rie_frecuencia", MySqlDbType.VarString).Value = _frecuencia;
-            objSelectCmd.Parameters.Add("tbl_cultivo_cul_id", MySqlDbType.Int32).Value = _fkCultivoId;
-            objSelectCmd.Parameters.Add("tbl_cultivo_tbl_parcela_par_id", MySqlDbType.Int32).Value = _fkParId;
+            objSelectCmd.Parameters.Add("v_id", MySqlDbType.Int32).Value = _idRiego;
+            objSelectCmd.Parameters.Add("v_tipo", MySqlDbType.VarString).Value = _tipo;
+            objSelectCmd.Parameters.Add("v_cantidad_agua", MySqlDbType.VarString).Value = _cantidad;
+            objSelectCmd.Parameters.Add("v_frecuencia", MySqlDbType.VarString).Value = _frecuencia;
+            objSelectCmd.Parameters.Add("vfk_cultivo_id", MySqlDbType.Int32).Value = _fkCultivoId;
+            objSelectCmd.Parameters.Add("vfk_tbl_par_id", MySqlDbType.Int32).Value = _fkParId;
             try
             {
                 row = objSelectCmd.ExecuteNonQuery();
@@ -88,9 +103,9 @@ namespace Data
 
             MySqlCommand objSelectCmd = new MySqlCommand();
             objSelectCmd.Connection = objPer.openConnection();
-            objSelectCmd.CommandText = "procDeleteWeather";
+            objSelectCmd.CommandText = "procDeleteIrrigation";
             objSelectCmd.CommandType = CommandType.StoredProcedure;
-            objSelectCmd.Parameters.Add("Rie_id", MySqlDbType.Int32).Value = _idRiego;
+            objSelectCmd.Parameters.Add("v_id", MySqlDbType.Int32).Value = _idRiego;
             try
             {
                 row = objSelectCmd.ExecuteNonQuery();
